210abf46f2fa471facbc14354d8056b571fb77ad
Leandro Lustosa <lustosa.leandro@gmail.com>
2016-03-23 21:41:32 +0100

    minor bugs corrected and added pixhawk mc_att as static method

---
+++ env.m
@@ -0,0 +1,9 @@
+%% ENV sets up the matlab environment
+%
+% user should call this function before any other in this library.
+
+INSTALL_PATH = 'D:\thesisCode\quadrotor-gnc';
+
+addpath(genpath(INSTALL_PATH));
+
+clear INSTALL_PATH;
